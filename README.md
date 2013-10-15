9f3baa0456ee63af9dd0041d653e64831c194c1d
jinhong.hu <jinhong.hu@qudian.so>
2013-10-15 22:58:43 +0800

    add getRandomPos function

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ It is also wonderful if you find some bug and get some issues.
 ### List
 * date2str  this function is used for formatting date from second to str
 * getDomainName  this function is used for getting domain name
-
+* getRandomPos get a random index in an array* getRandomPos get a random index in an array* getRandomPos get a random index in an array* getRandomPos get a random index in an array* getRandomPos get a random index in an array* getRandomPos get a random index in an array* getRandomPos get a random index in an array* getRandomPos get a random index in an array
--- funny-utils.js
+++ funny-utils.js
@@ -1,9 +1,9 @@
 /**
  * Format Date 
  * usage:
- *  date2str(new Date(data.qpoints[i].qtime*1000),"MM-dd hh:mm");
+ *  date2str(new Date(data.qpoints[i].qtime*1000),"yyyy-MM-dd hh:mm");
  */
-date2str = function(x,y) {
+date2str = function(x,y) {  
   var z = {M:x.getMonth()+1,d:x.getDate(),h:x.getHours(),m:x.getMinutes(),s:x.getSeconds()};
   y = y.replace(/(M+|d+|h+|m+|s+)/g,function(v) {
     return ((v.length>1?"0":"")+eval('z.'+v.slice(-1))).slice(-2)
@@ -16,6 +16,29 @@ date2str = function(x,y) {
  * eg: 'http://123.blog.163.com/123/123.html'
  */
 getDomainName = function(url_str) {
-  return url_str.match(/(\w+\.(?:com|cn|net|org|cc|so|be|me|io|edu|gov|info|tv|museum|coop|aero|
-    int|biz|name|pro|aero|travel|jobs|mobi|jp|hk))(?:\/|$)/)[1];
+  return url_str.match(/(\w+\.(?:com|cn|net|org|cc|so|be|me|io|edu|gov|info|tv|museum|coop|aero|int|biz|name|pro|aero|travel|jobs|mobi|jp|hk))(?:\/|$)/)[1];
+}
+
+/**
+ * Get a random index in an array.
+ * @param {Int} arrayLength. The length of an array.
+ * @param {Array} exceptPosArray. An array contains numbers 
+ *                you want to except in the array.
+ * @return {Int} return the random index of the array as you need.
+ */
+getRandomPos = function(arrayLenth, exceptPosArray) {
+  var pos, array = [];
+  exceptPosArray ? exceptPosArray = exceptPosArray : exceptPosArray = [];
+
+  for(var i = 0; i < arrayLenth; i++) {
+    array.push(i);
+  }
+
+  pos = Math.round(Math.random() * arrayLenth);
+  if(goog.array.contains(exceptPosArray, pos)) {
+    this.getRandomPos(arrayLenth, exceptPosArray);
+  }
+  else {
+    return pos;
+  }
 }
